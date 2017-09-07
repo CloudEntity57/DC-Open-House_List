c89fe98e6b678ddac929cccae44a208ea78b8d38
Hannah <hannah@rlathome.com>
2017-09-07 11:58:28 -0400

    commented out original

--- app.js
+++ app.js
@@ -33,9 +33,7 @@ app.use('/users', users);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('./client/build'));
 }
-
-//uncommented line 38 to test
-app.use('/',express.static('client/build'));
+//app.use('/',express.static('client/build'));
 app.use('/info', index);
 app.use('/',express.static('client/build'));
 
