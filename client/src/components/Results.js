a5ca6bda2973506e178d27ca0153334b357653fd
rlathome <31452089+rlathome@users.noreply.github.com>
2017-09-18 11:28:22 -0400

    test saturday

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -532,7 +532,7 @@ class Results extends Component{
         let today ='';
         switch(this.props.params.day){
           case 'saturday':
-          today = ' DSaturday';
+          today = ' DDSaturday';
           break;
           case 'sunday':
           today = ' Sunday';
