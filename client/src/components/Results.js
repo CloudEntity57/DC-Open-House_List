11e7412fd0b93a7a7da25b60142a50f002c6532f
Justin Levitch <justinlevitch@Justins-MacBook-Pro.local>
2017-09-24 02:25:26 -0400

    npm run biuild changes add

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -6,20 +6,12 @@ import currency from 'currency-formatter';
 import moment from 'moment';
 // import _ from "lodash";
 import ReactMap from './ReactMap';
-<<<<<<< HEAD
-// let apiKey = (process.env.REACT_APP_STATUS == 'development') ? "http://localhost:8080" : "https://infinite-brook-26465.herokuapp.com";
 
-// let apiKey="https://dcopenhouselist.herokuapp.com";
-
-let apiKey = "http://localhost:8080";
-=======
 // let apiKey = (process.env.REACT_APP_STATUS == 'development') ? "https://localhost:8080" : "https://vast-shore-14133.herokuapp.com";
 
 let apiKey="https://vast-shore-14133.herokuapp.com";
 
 // let apiKey = "https://localhost:8080";
->>>>>>> b0245cdd0b07383980950165820e4aa792227933
-
 
 class Results extends Component{
   constructor(props){
